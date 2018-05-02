5309ebb631f0b0cdde957fc5eed64e618140958b
jtluo05 <jtluo05@gmail.com>
2018-05-01 20:04:37 -0700

    Binary converter, sensitive keyboard

--- src/BinaryConverter.java
+++ src/BinaryConverter.java
@@ -4,12 +4,17 @@ import java.awt.event.MouseListener;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
 
 public class BinaryConverter implements MouseListener {
 	JButton button = new JButton();
 	JFrame frame = new JFrame();
 	JLabel label = new JLabel();
-
+JPanel panel=new JPanel();
+String input;
+JTextField tf=new JTextField(20);
 	public static void main(String[] args) {
 		BinaryConverter bc = new BinaryConverter();
 		bc.createUI();
@@ -17,18 +22,44 @@ public class BinaryConverter implements MouseListener {
 	}
 
 	void createUI() {
-		frame.show();
-		frame.add(label);
-		label.add(button);
+		frame.setVisible(true);
+		frame.add(panel);
+		panel.add(label);
+		panel.add(tf);
+		panel.add(button);
+		button.setText("convert");
 		frame.pack();
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		button.addMouseListener(this);
+		
+		frame.pack();
 	}
+	 String convert(String input) {
+         if(input.length() != 8){
+              JOptionPane.showMessageDialog(null, "Enter 8 bits, silly!!!");
+              return "-";
+         }
+         String binary = "[0-1]+";    //must contain numbers in the given range
+         if (!input.matches(binary)) {
+              JOptionPane.showMessageDialog(null, "Binary can only contain 1s or 0s, silly!!!");
+              return "-";
+         }
+         try {
+              int asciiValue = Integer.parseInt(input, 2);
+              char theLetter = (char) asciiValue;
+              return "" + theLetter;
+         } catch (Exception e) {
+              JOptionPane.showMessageDialog(null, "Enter a binary, silly!!!");
+              return "-";
+         }
+    }
 
 	@Override
 	public void mouseClicked(MouseEvent e) {
 		// TODO Auto-generated method stub
-
+String character=convert(tf.getText());
+label.setText(character);
+frame.pack();
 	}
 
 	@Override
